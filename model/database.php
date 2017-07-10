765ee3e1c7049ab813a5752c670d32afacea08ad
Rafael Correa <rc443@njit.edu>
2017-07-09 21:08:22 -0400

    I updated the host to show sql2.njit.edu and dbname, username, and password

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=rc443';
+    $username = 'rc443;
+    $password = 'Gpsc539F';
 
     try {
         $db = new PDO($dsn, $username, $password);
