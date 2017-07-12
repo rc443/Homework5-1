57c54248178e6c1bf08654ddb6868d0ab4613e1c
Rafael Correa <rc443@njit.edu>
2017-07-11 20:15:53 -0400

    Wrote code that creates the category table shown above with all of the category names in the first colum and Delete buttons in the second column

--- product_manager/product_list.php
+++ product_manager/product_list.php
@@ -1,53 +1,36 @@
-<?php include '../view/header.php'; ?>
 <main>
-    <h1>Product List</h1>
+	<h1>Category List</h1>
+	<table>
+		<tr>
+			<th>Name</th>
+			<th>&nbsp;</th>
+		</tr>
+		<?php foreach ($categories as $category) : ?>
+		<tr>
+			<td><?php echo $category['categoryName'}; ?></td>
+			<td>
+				<form action="index.php" method="post">
+					<input type="hidden" name="action" value="delete_category" />
+					<input type="hidden" name="category_id"
+						value="<?php echo $category['categoryID']; ?>"/>
+					<Input type="submit" value="Delete"?>
+				</form>
+			</td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
 
-    <aside>
-        <!-- display a list of categories -->
-        <h2>Categories</h2>
-        <nav>
-        <ul>
-        <?php foreach ($categories as $category) : ?>
-            <li>
-            <a href="?category_id=<?php echo $category['categoryID']; ?>">
-                <?php echo $category['categoryName']; ?>
-            </a>
-            </li>
-        <?php endforeach; ?>
-        </ul>
-        </nav>
-    </aside>
+	<h2>Add Category</h2>
+	<form id="add_category_form"
+		action="index.php" method="post">
+	     <input type="hidden" name="action" value="add_category" />
 
-    <section>
-        <!-- display a table of products -->
-        <h2><?php echo $category_name; ?></h2>
-        <table>
-            <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th class="right">Price</th>
-                <th>&nbsp;</th>
-            </tr>
-            <?php foreach ($products as $product) : ?>
-            <tr>
-                <td><?php echo $product['productCode']; ?></td>
-                <td><?php echo $product['productName']; ?></td>
-                <td class="right"><?php echo $product['listPrice']; ?></td>
-                <td><form action="." method="post">
-                    <input type="hidden" name="action"
-                           value="delete_product">
-                    <input type="hidden" name="product_id"
-                           value="<?php echo $product['productID']; ?>">
-                    <input type="hidden" name="category_id"
-                           value="<?php echo $product['categoryID']; ?>">
-                    <input type="submit" value="Delete">
-                </form></td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
-        <p class="last_paragraph">
-            <a href="?action=show_add_form">Add Product</a>
-        </p>
-    </section>
+	     <label>Name:</label>
+	     <input type="text" name="name" />
+	     <input type="submit" value="Add" />
+	</form>
+
+	<p><a href="index.php?action=list_products">List Products</a></p>
+
 </main>
 <?php include '../view/footer.php'; ?>
